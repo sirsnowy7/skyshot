a89de6ebb06d7ef9c1684115437290668350f8d6
sirsnowy7 <savagesans@gmail.com>
2017-12-14 04:55:44 +0000

    Added background
    Finished background work with temporary graphics, probably. Added
    bullets and a shooting mechanic.

--- main.py
+++ main.py
@@ -2,18 +2,24 @@
 # Simple game made in Python with Pygame. Uses a font from Google
 # and original graphics. Currently in alpha, and is not playable.
 
-import sys, time, pygame
+import sys, pygame
 from pygame.locals import *
 pygame.init()
 
 # gameplay
 location = ( 640 / 2 ) - 16
 accel = 2
+acc_m = 8
+tick = 0
 ctls = { "l": K_LEFT, "r": K_RIGHT, "f": K_z, "s": K_x }
 keys = { "l": False, "r": False, "f": False, "s": False }
+keys_s = { "l": False, "r": False, "f": False, "s": False }
 play_s = { "health": 20, "damage": 2 }
-enemy1 = { "health": 20, "damage": 4, "sprite": "assets/enemy1.png" }
-enemy2 = { "health": 30, "damage": 2, "sprite": "assets/enemy2.png" }
+enemies = { "enemy1": { "health": 20, "damage": 4, "sprite": "assets/enemy1.png" },
+  "enemy2": { "health": 30, "damage": 2, "sprite": "assets/enemy2.png" } }
+p_bullets = []
+bullets = []
+clock = pygame.time.Clock()
 
 # colors
 bg_color = 244, 244, 244
@@ -22,17 +28,20 @@ dark = 20, 20, 20
 # screen
 s_size = width, height = 640, 480
 screen = pygame.display.set_mode(s_size)
+pygame.display.set_caption("Skyshot")
 
 # images
 player = pygame.image.load("assets/ship.png")
 enemy_w = pygame.image.load("assets/enemy1.png")
 enemy_t = pygame.image.load("assets/enemy2.png")
-pygame.display.set_caption("Skyshot")
+bullet_p = pygame.image.load("assets/bullet1.png")
+bullet_e = pygame.image.load("assets/bullet2.png")
+land = { "img": pygame.image.load("assets/forest.png"), "pos1": 0, "pos2":-480 }
+cloud = { "img": pygame.image.load("assets/clouds.png"), "pos1": -480, "pos2": -1440 }
+cloud_ac = 16
 
 # font
 vt = pygame.font.Font("assets/font/VT323-Regular.ttf", 32)
-
-clock = 0
 
 def game_over():
   screen.fill(bg_color)
@@ -49,8 +58,14 @@ def game_over():
         if event.key == K_ESCAPE:
           sys.exit()
 
+def opening():
+  print("wip")
+
 while True:
-  clock += 1
+  tick += 1
+
+  for key in keys_s:
+    keys_s[key] = False
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -67,6 +82,7 @@ while True:
       for ctl in ctls:
         if event.key == ctls[ctl]:
           keys[ctl] = True
+          keys_s[ctl] = True
 
   if play_s["health"] <= 0:
     game_over()
@@ -83,22 +99,53 @@ while True:
     accel -= 2
   elif accel <= -1:
     accel += 2
+  
+  if keys_s["f"] == True:
+    p_bullets.append({"h": 400, "w": location + 8})
 
-  if accel >= 10:
-    accel = 10
-  elif accel <= -10:
-    accel = -10
+  if accel >= acc_m:
+    accel = acc_m
+  elif accel <= -acc_m:
+    accel = -acc_m
 
   if location > 600:
     location = 600
   elif location < 40:
     location = 40
+  
+  cloud["pos1"] += cloud_ac
+  if cloud["pos1"] >= 480:
+    cloud["pos1"] = -1440
+  
+  cloud["pos2"] += cloud_ac
+  if cloud["pos2"] >= 480:
+    cloud["pos2"] = -1440
 
+  land["pos1"] += cloud_ac / 4
+  if land["pos1"] >= 480:
+    land["pos1"] = -480
+
+  land["pos2"] += cloud_ac / 4
+  if land["pos2"] >= 480:
+    land["pos2"] = -480
+  
   r_location = location, 400
   
+  # drawing
   screen.fill(bg_color)
-  screen.blit(pygame.transform.scale(player, (32, 64)), r_location)
-  pygame.display.flip()
   
-  print(clock, keys["l"], keys["r"], keys["f"], keys["s"], accel )
-  time.sleep(0.1)
+  screen.blit(land["img"], (0, land["pos1"]))  
+  screen.blit(land["img"], (0, land["pos2"]))
+  screen.blit(cloud["img"], (0, cloud["pos1"]))
+  screen.blit(cloud["img"], (0, cloud["pos2"]))
+  
+  i = 0
+  while i < len(p_bullets):
+    p_bullets[i]["h"] -= 8
+    screen.blit(pygame.transform.scale(bullet_p, (8, 16)), (p_bullets[i]["w"], p_bullets[i]["h"]))
+    i += 1
+
+  screen.blit(pygame.transform.scale(player, (32, 48)), r_location)
+  pygame.display.flip()
+
+  clock.tick(30)
